7220beb556e0d83a210d8a250cfe8c3c1810acc6
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-06-15 08:59:36 +0200

    Initial test harness

---
+++ tests/src/LoaderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\Composer\ClassLoader\Tests\LoaderTest.
+ */
+
+namespace Drupal\Composer\ClassLoader\Tests;
+use Drupal\Composer\ClassLoader\Loader;
+
+/**
+ * Class LoaderTest
+ * @coversDefaultClass \Drupal\Composer\ClassLoader\Loader
+ * @package Drupal\Composer\ClassLoader\Tests
+ */
+class LoaderTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Tests that Loader::autoload() works properly.
+   * @covers ::autoload()
+   */
+  public function testAutoload() {
+    Loader::setClassMap([
+      '\\Acme' => '../data/acme.inc'
+    ]);
+    Loader::setSeed(__DIR__);
+    $this->assertTrue(Loader::autoload('Acme'));
+  }
+
+}
