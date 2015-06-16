bb1fa9263d5fd6857d07133af215b7e4f5c1785c
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-06-16 12:20:50 +0200

    Added test class

---
+++ tests/src/AutoloaderBootstrapTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\Composer\ClassLoader\Tests\AutoloaderBootstrapTest.
+ */
+
+namespace Drupal\Composer\ClassLoader\Tests;
+
+/**
+ * Class AutoloaderBootstrapTest
+ *
+ * @coversDefaultClass Drupal\Composer\ClassLoader\AutoloaderBootstrap
+ *
+ * @package Drupal\Composer\ClassLoader\Tests
+ */
+class AutoloaderBootstrapTest extends \PHPUnit_Framework_TestCase {
+
+}
