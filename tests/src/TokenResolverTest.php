4578cd1dcc60e23a14495ea019ebe39d257956ee
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-06-16 11:41:50 +0200

    Test resolve method

--- tests/src/TokenResolverTest.php
+++ tests/src/TokenResolverTest.php
@@ -30,4 +30,43 @@ class TokenResolverTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($tokenized_path, $value);
   }
 
+  /**
+   * Tests that ::resolve() is working.
+   *
+   * @dataProvider resolveProvider
+   *
+   * @covers ::resolve()
+   */
+  public function test_resolve($path, $expected) {
+    $resolver = new TokenResolver($path);
+    $finder = $resolver->resolve();
+    $this->assertInstanceOf($expected, $finder);
+  }
+
+  /**
+   * Provider method for test_resolve.
+   */
+  public static function resolveProvider() {
+    return [
+      // 1. Path not tokenized.
+      ['data/acme.inc', '\Drupal\Composer\ClassLoader\Discovery\PathFinderNull'],
+      // 2. Core path.
+      ['DRUPAL_ROOT/file.inc', '\Drupal\Composer\ClassLoader\Discovery\PathFinderCore'],
+      // 3. Contrib path.
+      ['DRUPAL_CONTRIB<testmodule>/testmodule.info', '\Drupal\Composer\ClassLoader\Discovery\PathFinderContrib'],
+    ];
+  }
+
+  /**
+   * Tests that ::resolve() is working.
+   *
+   * @covers ::resolve()
+   */
+  public function test_resolve_unexisting() {
+    // 4. Unexisting path without a real token.
+    $resolver = new TokenResolver('Lorem');
+    $finder = $resolver->resolve();
+    $this->assertNull($finder);
+  }
+
 }
