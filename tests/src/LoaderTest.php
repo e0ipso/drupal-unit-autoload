99e72b1e1f9082b2792fce619f83312d78611abc
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2015-06-16 08:50:38 +0200

    Add more protected coverage

--- tests/src/LoaderTest.php
+++ tests/src/LoaderTest.php
@@ -20,6 +20,7 @@ class LoaderTest extends \PHPUnit_Framework_TestCase {
    *
    * @covers ::autoload()
    * @covers ::autoloadPaths()
+   * @covers ::unprefixClass()
    */
   public function test_autoload() {
     Loader::setClassMap([
