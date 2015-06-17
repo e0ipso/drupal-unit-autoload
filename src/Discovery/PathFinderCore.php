0df82163e13280c930a9f2f6aedd1ce8957ac8c5
Christian Lopez Espinola <penyaskito@computer.org>
2015-06-17 13:25:26 +0200

    Cleaned up path creation and drupal checks.

--- src/Discovery/PathFinderCore.php
+++ src/Discovery/PathFinderCore.php
@@ -17,19 +17,18 @@ class PathFinderCore extends PathFinderBase implements PathFinderInterface {
   public function find($seed) {
     $seed = realpath($seed);
     // Try to create the iterator with the seed.
-    try {
+    $directory = NULL;
+    if (is_dir($seed)) {
       $directory = new \DirectoryIterator($seed);
     }
-    catch (\RuntimeException $e) {
-      // If the seed was not a directory, then get the parent directory.
-      $path_info = pathinfo($seed);
-      $directory = new \DirectoryIterator($path_info['dirname']);
+    else {
+      $directory = new \DirectoryIterator(dirname($seed));
     }
     // Starting at the directory containing the seed path, we go one directory
     // up and up and up until we reach the Drupal root.
     do {
       if ($this->isDrupalRoot($directory)) {
-        return $this->cleanDirPath($directory->getPathName()) . $this->path;
+        return dirname($this->cleanDirPath($directory->getPathName())) . $this->path;
       }
     }
     while ($directory = $this->getParentDirectory($directory));
@@ -54,15 +53,14 @@ class PathFinderCore extends PathFinderBase implements PathFinderInterface {
     // operator.
     $d = clone $directory;
     // Check if there is a COPYRIGHT.txt file in the directory.
-    foreach ($d as $item) {
-      if (!$item->isFile() || $item->getFilename() != 'COPYRIGHT.txt') {
-        continue;
-      }
+    $copyrightPath = $d->getPath() . DIRECTORY_SEPARATOR . 'COPYRIGHT.txt';
+    $check = file_exists($copyrightPath);
+    if ($check) {
       // Make sure that the COPYRIGHT.txt file corresponds to Drupal.
-      $line = fgets(fopen($item->getPathname(), 'r'));
-      return strpos($line, 'All Drupal code is Copyright') === 0;
+      $line = fgets(fopen($copyrightPath, 'r'));
+      $check = (strpos($line, 'All Drupal code is Copyright') === 0);
     }
-    return FALSE;
+    return $check;
   }
 
   /**
@@ -79,8 +77,7 @@ class PathFinderCore extends PathFinderBase implements PathFinderInterface {
    */
   protected function getParentDirectory(\DirectoryIterator $directory) {
     // Get the path name of the directory.
-    $path_name = $directory->getPathname();
-    $path_name = $this->cleanDirPath($path_name);
+    $path_name = dirname($directory->getPathname());
 
     // Get the parent directory and return a DirectoryIterator.
     $path_info = pathinfo($path_name);
